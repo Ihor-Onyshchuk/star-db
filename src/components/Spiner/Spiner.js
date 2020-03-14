e0c84be0555c7890efdfba86fc9d6f6ba6a9371a
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-03-14 18:15:34 +0200

    added: ErrorIndicator, Spiner

---
+++ src/components/Spiner/Spiner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Spiner.css";
+
+const Spiner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-dhzdnufln3">
+      <div class="ldio-zxu9a091tk">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spiner;
