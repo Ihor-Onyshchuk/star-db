b63f7c3c417d4674050c90896700633b729097f8
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-03-30 11:52:38 +0300

    Updates

---
+++ precache-manifest.1e915555f8aa52fa2a67ca0f1e8c2655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bbf8924e103e2b9f4e11b595ce08d2",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "14b0acc3d95dc7cc320c",
+    "url": "/star-db/static/css/2.b5efd36a.chunk.css"
+  },
+  {
+    "revision": "791754f46f98337ea287",
+    "url": "/star-db/static/css/main.9d582d2c.chunk.css"
+  },
+  {
+    "revision": "14b0acc3d95dc7cc320c",
+    "url": "/star-db/static/js/2.55e8ba98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-db/static/js/2.55e8ba98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791754f46f98337ea287",
+    "url": "/star-db/static/js/main.f8b346ed.chunk.js"
+  },
+  {
+    "revision": "ba152e2bbb51542a9c05",
+    "url": "/star-db/static/js/runtime-main.e01fc811.js"
+  }
+]);
